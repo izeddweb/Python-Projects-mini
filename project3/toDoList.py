f1a01b92e88580408c3a1c5cbea3b8a76d0be145
izeddweb <abzedd61@gmail.com>
2024-06-21 19:28:27 +0200

    echo not fixed

--- project3/toDoList.py
+++ project3/toDoList.py
@@ -1,35 +1,40 @@
-reponse = input("do you want to add ,view , remove or exit \n")
 
 commands = ["add", "view", "remove", "exit"]
 notes = ["zedd", "ab", "azer"]
 score = 0
 confirmCommand = ""
 
+while True :
+    reponse = input("do you want to add ,view , remove or exit \n")
 
-if reponse == "add":
-    score += 1
-    confirmCommand = input("item to add \n")
-    notes.append(f"{confirmCommand}-{score}")
-    print(notes)
+    if reponse == "add":
+        score += 1
+        confirmCommand = input("item to add \n")
+        notes.append(f"{confirmCommand}-{score}")
+        print(notes)
 
-elif reponse == "remove":
-    confirmCommand = input(" item to remove \n")
-    for note in notes:
-        if note == confirmCommand:
-            notes.remove(note)
-            print(f'-{ note} --- {notes}')
-elif reponse == "view":
-    for note in notes :
-        score += 1  
-        print(f' item number{ score } : {note} ')
+    elif reponse == "remove":
+        confirmCommand = input(" item to remove \n")
+        for note in notes:
+            if note == confirmCommand:
+                notes.remove(note)
+                print(f'-{ note} --- {notes}')
+            elif note != confirmCommand :
+                print('item not found')
+    elif reponse == "view":
+        for note in notes :
+            score += 1  
+            print(f' item number{ score } : {note} ')
 
-elif reponse == "exit" :
-    confirmCommand = input(" do you want to exit !! \n")
-    if confirmCommand == 'yes':
-        print(' ok N A K A M A')
-
-    
-else :
-    print('------this command not found')
+    elif reponse == "exit" :
+        confirmCommand = input(" do you want to exit !! \n")
+        if confirmCommand == 'yes':
+            print(' OH NOOOO N A K A M A.  !!!!!!')
+            break
+        else :
+            print(' ok N A K A M A')
+        
+    else :
+        print('------this command not found')
 
 
